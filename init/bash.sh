54917aa0657b67fae2ccb74471f013abd35f1c30
yhli <liyunhai1016@hotmail.com>
2018-12-04 21:20:45 +0800

    Enabled automatic mode by default

--- init/bash.sh
+++ init/bash.sh
@@ -5,7 +5,7 @@ PM_ROOT=$HOME/proj/pmod
 export PATH=$PM_ROOT/bin:$PATH
 export PYTHONPATH=$PM_ROOT:$PYTHONPATH
 export PM_LOADED_MODULES=""
-export PM_AUTO_MODE=0
+export PM_AUTO_MODE=1
 
 # Setup the 'module' command
 if [ ! -x "$PM_ROOT/bin/modcmd.py" ]; then
